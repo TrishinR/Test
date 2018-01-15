adb7a9a9ec5b5bafb1df155a6facf18ae5b4e461
TrishinR <trishin.IT@gmail.com>
2018-01-15 10:40:06 +0200

    Update player.js

--- js/player.js
+++ js/player.js
@@ -7,13 +7,11 @@ function pauseVideo()
 // Play button, required by browsers to grab user interaction before autoplaying videos.
 document.querySelector('#play-button').addEventListener("click", function(e){
 	startStuff();
-//this.style.display = 'none';
 }, false);
 	  
 // Pause video button.
 document.querySelector('#pause-button').addEventListener("click", function(e){
 	pauseVideo();
-//this.style.display = 'none';
 }, false);
 
 function startStuff(){
